fde39fce7546ee3f17d4c1d9f006bc62e4590d41
Domenica Katherine Amores Cedeño <domenicaamores2006@gmail.com>
2026-03-27 23:47:07 -0500

    Interfaz de Activities + Conectada a Main Activity

--- Proyecto_Computronic_EcoShielEC/EcoShieldEC/app/src/main/java/espol/spacehack/ecoshieldec/ui/MainActivity.java
+++ Proyecto_Computronic_EcoShielEC/EcoShieldEC/app/src/main/java/espol/spacehack/ecoshieldec/ui/MainActivity.java
@@ -2,20 +2,23 @@ package espol.spacehack.ecoshieldec.ui;
 
 import android.os.Bundle;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import espol.spacehack.ecoshieldec.R;
-import espol.spacehack.ecoshieldec.ui.fragments.*;
-
-
+import espol.spacehack.ecoshieldec.ui.fragments.ActivitiesFragment;
+import espol.spacehack.ecoshieldec.ui.fragments.MapDashboardFragment;
+import espol.spacehack.ecoshieldec.ui.fragments.RewardsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Carga el cascarón con el menú inferior
+        setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
 
@@ -23,21 +26,23 @@ public class MainActivity extends AppCompatActivity {
             int itemId = item.getItemId();
 
             if (itemId == R.id.nav_map) {
-                // Carga la SECCIÓN 1 (Matías)
                 loadFragment(new MapDashboardFragment());
                 return true;
+
+            } else if (itemId == R.id.nav_activities) {
+                loadFragment(new ActivitiesFragment());
+                return true;
+
             } else if (itemId == R.id.nav_rewards) {
-                // Carga la SECCIÓN 5 (Doménica)
                 loadFragment(new RewardsFragment());
                 return true;
+
             } else {
-                // Para las demás, mostramos un mensaje temporal
                 Toast.makeText(this, "Sección en desarrollo", Toast.LENGTH_SHORT).show();
                 return true;
             }
         });
 
-        // Al abrir la app, cargamos TU MAPA por defecto
         if (savedInstanceState == null) {
             bottomNav.setSelectedItemId(R.id.nav_map);
         }
--- Proyecto_Computronic_EcoShielEC/EcoShieldEC/app/src/main/java/espol/spacehack/ecoshieldec/ui/fragments/ActivitiesFragment.java
+++ Proyecto_Computronic_EcoShielEC/EcoShieldEC/app/src/main/java/espol/spacehack/ecoshieldec/ui/fragments/ActivitiesFragment.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import espol.spacehack.ecoshieldec.R;
 
-public class RewardsFragment extends Fragment {
+public class ActivitiesFragment extends Fragment {
+
+    public ActivitiesFragment() {
+    }
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_rewards, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_activities, container, false);
     }
 }
